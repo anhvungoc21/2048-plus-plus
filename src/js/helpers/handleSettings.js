d16ce7f5860c5d9f50b44d52b6c9f017a5794dd1
Anh Vu <anhvungoc.21@gmail.com>
2022-07-07 15:07:27 -0500

    Updated tick marks

--- src/js/helpers/handleSettings.js
+++ src/js/helpers/handleSettings.js
@@ -4,11 +4,15 @@ const menuColors = document.getElementById("collapse--colors");
 import setupGame from "../script.js";
 import { setGridSize, setColor } from "../config.js";
 
-function handleToggleBoardSize(e, gridElement) {
+function handleToggleBoardSize(e) {
   const choice = e.target.closest(".collapse__sublink");
   if (choice) {
+    // Display tick icon
+    const parent = choice.closest(".collapse__menu");
+    handleTickIcon(choice, parent);
+
+    // Update config and reconstruct board
     const gridSize = parseInt(choice.dataset.boardSize);
-    // Update in config and reconstruct board
     setGridSize(gridSize);
     setupGame();
   }
@@ -16,13 +20,27 @@ function handleToggleBoardSize(e, gridElement) {
 
 function handleToggleColors(e) {
   const choice = e.target.closest(".collapse__sublink");
-  choice && setColor(choice.dataset.color);
+  if (choice) {
+    // Display tick icon
+    const parent = choice.closest(".collapse__menu");
+    handleTickIcon(choice, parent);
+
+    // Update config and change color
+    const color = choice.dataset.color;
+    setColor(color);
+  }
+}
+
+function handleTickIcon(element, parent) {
+  const thisTickIcon = element.querySelector(".check__icon");
+  const otherTickIcons = parent.querySelectorAll(".check__icon");
+  if (thisTickIcon && otherTickIcons) {
+    otherTickIcons.forEach((icon) => icon.classList.add("hidden"));
+    thisTickIcon.classList.remove("hidden");
+  }
 }
 
 export default function handleCollapseMenu() {
-  const gameBoard = document.getElementById("game-board");
-  menuBoardSize.addEventListener("click", (e) =>
-    handleToggleBoardSize(e, gameBoard)
-  );
+  menuBoardSize.addEventListener("click", (e) => handleToggleBoardSize(e));
   menuColors.addEventListener("click", (e) => handleToggleColors(e));
 }
