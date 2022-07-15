98983af36a9e494ada6e5864e2ace288f9265012
Anh Vu <anhvungoc.21@gmail.com>
2022-07-15 18:28:43 -0500

    Rebuilt for deployment

--- src/js/helpers/handleUser.js
+++ src/js/helpers/handleUser.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-// import { createAccount, getAccount } from "../db/db.js";
-=======
 import { createAccount, getAccount } from "../db/db.js";
 
->>>>>>> 3edb6de58669b141d09f20f0d377a44601a3a309
 import {
   getLoggedIn,
   setLoggedIn,
