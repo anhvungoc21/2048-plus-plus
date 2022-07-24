5d0b0f0792b5930d307cfb2253e79d9a7ca07885
Anh Vu <anhvungoc.21@gmail.com>
2022-07-23 19:34:21 -0500

    Added combos

--- src/js/helpers/handleScore.js
+++ src/js/helpers/handleScore.js
@@ -1,5 +1,9 @@
+import { setCombo, getCombo, INC_PER_COMBO } from "../gameState.js";
+
 const scoreContainer = document.querySelector(".score-container");
 const bestScoreContainer = document.querySelector(".best-container");
+const comboContainer = document.querySelector(".combo-container");
+const comboBar = document.querySelector(".combo-bar");
 
 /**
  * Updates score everytime a tile is merged
@@ -34,4 +38,35 @@ function updateScore(scoreAdd) {
   }, 600); /* Animation time is 600ms */
 }
 
-export { updateScore };
+function updateCombo(combosCount) {
+  if (getCombo()) return;
+  const currentWidth = parseInt(
+    getComputedStyle(comboContainer).getPropertyValue("--width")
+  );
+  if (currentWidth == 100) return;
+
+  const widthInc = combosCount * INC_PER_COMBO;
+  const updatedWidth = widthInc + currentWidth;
+  if (updatedWidth >= 100) {
+    comboContainer.style.setProperty("--width", "100%");
+
+    // Set combo state, animations.
+    setCombo(true);
+    comboContainer.style.setProperty("--transition-time", "10s");
+    comboContainer.style.setProperty("--width", "0%"); // setInterval's callback doesn't affect this
+    comboBar.style.background = "var(--black-color)";
+    comboBar.classList.add("blinker");
+
+    // Combo state persists for 10 seconds
+    setTimeout(() => {
+      setCombo(false);
+      comboContainer.style.setProperty("--transition-time", "0.25s");
+      comboBar.style.background = "var(--white-color)";
+      comboBar.classList.remove("blinker");
+    }, 10000);
+  } else {
+    comboContainer.style.setProperty("--width", `${updatedWidth}%`);
+  }
+}
+
+export { updateScore, updateCombo };
